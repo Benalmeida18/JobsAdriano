527ff3f49edc6fac54dd561bb0f4bf95dd7d8c73
Stanley Andrade <45856878+StanleyAndrade@users.noreply.github.com>
2023-08-29 17:11:03 -0300

    ain

--- src/assets/js/pagamento.js
+++ src/assets/js/pagamento.js
@@ -1,6 +1,9 @@
-// SDK MercadoPago.js
-<script src="https://sdk.mercadopago.com/js/v2"></script>
-
-
 const mp = new MercadoPago('APP_USR-1106e10c-b1fa-49b3-ab3d-209febceb7ff');
+
+
+mp.bricks().create("wallet", "wallet_container", {
+    initialization: {
+        preferenceId: "wallet_container",
+    },
+});
 
